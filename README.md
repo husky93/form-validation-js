baf28fd21c15df37f7a0416886f201b195f4ced9
Maciej Sroka <sroka.maciej93@gmail.com>
2022-07-26 17:40:58 +0200

    Setup project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # form-validation-js
+
+Form validation using [Constraint validation API](https://developer.mozilla.org/en-US/docs/Web/API/Constraint_validation).
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'commonjs2',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+};
