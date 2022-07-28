caaba50f036fec42e34c90a1f251d78014ce88f6
Maciej Sroka <sroka.maciej93@gmail.com>
2022-07-28 12:50:03 +0200

    Add zip code input validation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import addSelectOptions from './modules/countries-input';
 import validateForm from './modules/validate';
 import './assets/style.css';
 
-validateForm.validateEmail();
+validateForm.initializeValidation();
 
 addSelectOptions();
--- src/modules/validate.js
+++ src/modules/validate.js
@@ -1,44 +1,60 @@
 const validateForm = (() => {
   const form = document.querySelector('form');
   const email = document.getElementById('mail');
+  const zip = document.getElementById('zip');
+  const zipError = document.querySelector('.error--zip');
   const emailError = document.querySelector('.error--email');
 
   const showError = () => {
     switch (true) {
-      case email.validity.valueMissing:
-        emailError.textContent = 'You need to enter e-mail address.';
-        break;
       case email.validity.typeMismatch:
         emailError.textContent = 'Entered value needs to be e-mail address';
         break;
       case email.validity.tooShort:
         emailError.textContent = `Email should be at least ${email.minLength} characters, you entered ${email.value.length}`;
         break;
+      case zip.validity.rangeUnderflow:
+        zipError.textContent = `Zip code is too short it should have exactly ${zip.minLength} numbers`;
+        break;
+      case zip.validity.rangeOverflow:
+        zipError.textContent = `Zip code is too long it should have exactly ${zip.maxLength} numbers`;
+        break;
+      case email.validity.valueMissing:
+        emailError.textContent = 'You need to enter e-mail address.';
+        break;
+      case zip.validity.valueMissing:
+        zipError.textContent = 'You need to enter a zip code.';
+        break;
       default:
         break;
     }
   };
 
-  (() => {
+  const validateInput = (input, errorBox) => {
+    input.addEventListener('input', () => {
+      if (input.validity.valid) {
+        const msgBox = errorBox;
+        msgBox.textContent = '';
+      } else showError();
+    });
+  };
+
+  const addSubmitHandler = () => {
     form.addEventListener('submit', (e) => {
-      if (!email.validity.valid) {
+      if (!email.validity.valid || !zip.validity.valid) {
         showError();
         e.preventDefault();
       }
     });
-  })();
-
-  const validateEmail = () => {
-    email.addEventListener('input', () => {
-      if (email.validity.valid) {
-        emailError.textContent = '';
-        emailError.className.toggle('error');
-      } else {
-        showError();
-      }
-    });
   };
-  return { validateEmail };
+
+  const initializeValidation = () => {
+    validateInput(email, emailError);
+    validateInput(zip, zipError);
+    addSubmitHandler();
+  };
+
+  return { initializeValidation };
 })();
 
 export default validateForm;
