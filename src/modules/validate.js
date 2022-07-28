2c53168847203f81549709fb43277dc71a1d4c06
Maciej Sroka <sroka.maciej93@gmail.com>
2022-07-28 14:05:07 +0200

    Add password validation

--- src/modules/validate.js
+++ src/modules/validate.js
@@ -2,8 +2,14 @@ const validateForm = (() => {
   const form = document.querySelector('form');
   const email = document.getElementById('mail');
   const zip = document.getElementById('zip');
+  const pass = document.getElementById('pass');
+  const confPass = document.getElementById('confpass');
   const zipError = document.querySelector('.error--zip');
   const emailError = document.querySelector('.error--email');
+  const passError = document.querySelector('.error--pass');
+  const confPassError = document.querySelector('.error--confpass');
+  let passwordsMatching = false;
+  let passCorrectPattern = false;
 
   const showError = () => {
     switch (true) {
@@ -19,15 +25,48 @@ const validateForm = (() => {
       case zip.validity.rangeOverflow:
         zipError.textContent = `Zip code is too long it should have exactly ${zip.maxLength} numbers`;
         break;
+      case pass.validity.patternMismatch:
+        passError.textContent =
+          'Minimum eight characters, at least one uppercase letter, one lowercase letter and one number';
+        break;
+      case !passwordsMatching:
+        confPassError.textContent = 'Passwords are not matching';
+        break;
       case email.validity.valueMissing:
         emailError.textContent = 'You need to enter e-mail address.';
         break;
       case zip.validity.valueMissing:
         zipError.textContent = 'You need to enter a zip code.';
         break;
+      case pass.validity.valueMissing:
+        passError.textContent = 'You need to enter a password.';
+        break;
+      case confPass.validity.valueMissing:
+        confPassError.textContent = 'You need to confirm the password.';
+        break;
       default:
         break;
     }
+  };
+
+  const isValueMatching = (input, inputTwo) => {
+    if (input.value === inputTwo.value) return true;
+    return false;
+  };
+
+  const checkPasswordMatch = () => {
+    const inputs = [pass, confPass];
+    inputs.forEach((input) =>
+      input.addEventListener('input', () => {
+        passwordsMatching = isValueMatching(pass, confPass);
+        if (!passwordsMatching) {
+          confPass.setCustomValidity('Passwords are not matching');
+        } else {
+          confPass.setCustomValidity('');
+          confPassError.textContent = '';
+        }
+      })
+    );
   };
 
   const validateInput = (input, errorBox) => {
@@ -41,7 +80,13 @@ const validateForm = (() => {
 
   const addSubmitHandler = () => {
     form.addEventListener('submit', (e) => {
-      if (!email.validity.valid || !zip.validity.valid) {
+      if (
+        !email.validity.valid ||
+        !zip.validity.valid ||
+        !pass.validity.valid ||
+        !confPass.validity.valid ||
+        !passwordsMatching
+      ) {
         showError();
         e.preventDefault();
       }
@@ -51,6 +96,9 @@ const validateForm = (() => {
   const initializeValidation = () => {
     validateInput(email, emailError);
     validateInput(zip, zipError);
+    validateInput(pass, passError);
+    validateInput(confPass, confPassError);
+    checkPasswordMatch();
     addSubmitHandler();
   };
 
